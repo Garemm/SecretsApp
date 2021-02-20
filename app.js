9d4375121fc71c89136996e65d9ec9cdfe130942
unknown <garemm27@gmail.comgit config --global user.name Jonnemman>
2021-02-20 00:34:08 -0600

    Add environment add

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt= require('mongoose-encryption');
+const md5= require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -20,8 +18,6 @@ const userSchema= new mongoose.Schema({ //poniendo Schema para poner el tipo de 
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User= new mongoose.model("User", userSchema);//creando una constante de model para la función que el usuario desempeña
 
@@ -40,7 +36,7 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser= new User({email: req.body.username, password: req.body.password});
+  const newUser= new User({email: req.body.username, password: md5(req.body.password)});
   newUser.save(function(err){
     if(err){
       console.log(err);
@@ -52,7 +48,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
 const username=req.body.username;
-const password= req.body.password;
+const password= md5(req.body.password);
 
 User.findOne({email: username}, function (err, foundUser){
   if(err){
